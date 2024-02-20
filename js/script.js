4946c04bd3221d60afb3993e789036ff38b19a5d
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-02-20 14:46:03 +0100

    adding new tasks to list

--- js/script.js
+++ js/script.js
@@ -1,14 +1,49 @@
 {
     const tasks = [];
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+                <li class='tasksList__listItem ${task.done === true ? 'tasksList__listItem--done' : ""}'>
+                <button>✔</button> ${task.content} <button>X</button>
+                </li>`
+        };
+
+        document.querySelector(".js-tasksList").innerHTML = htmlString;
     };
 
+
+    const addNewTaskContent = (newTaskContent) => {
+        tasks.push(
+            {
+                content: newTaskContent,
+            },
+        );
+
+        render();
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-addNewTask__input").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTaskContent(newTaskContent)
+    };
+
+
     const init = () => {
+        render();
 
         const formElement = document.querySelector(".js-form");
         formElement.addEventListener("submit", onFormSubmit);
     };
+
+
     init();
 };
