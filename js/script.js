c7a3b70c313388d4c3dd9d3b6166ef5730bd8859
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-02-20 13:27:45 +0100

    prevented reloading page

---
+++ js/script.js
@@ -0,0 +1,14 @@
+{
+    const tasks = [];
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    const init = () => {
+
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+    init();
+};
